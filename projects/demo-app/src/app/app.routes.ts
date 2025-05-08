72004471eb98c94555c059bf4c1c393677824b59
MrNizzy <3134183631y@gmail.com>
2025-05-08 17:58:59 -0500

    feat: mejorar configuración de rutas y formatear app.config.ts

--- projects/demo-app/src/app/app.routes.ts
+++ projects/demo-app/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+    title: 'Inicio - NGX Material Extra',
+  },
+  {
+    path: 'avatars',
+    loadComponent: () =>
+      import('./pages/avatars/avatars.component').then(
+        (m) => m.AvatarsComponent
+      ),
+    title: 'Avatars - NGX Material Extra',
+  },
+];
