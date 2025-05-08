966bb1421fdabf92ca0f0020cd0ae612bd607e92
MrNizzy <3134183631y@gmail.com>
2025-05-08 17:58:27 -0500

    feat: reestructurar app.component para incluir contenedor de contenido y navbar

--- projects/demo-app/src/app/app.component.ts
+++ projects/demo-app/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgxMaterialExtraComponent } from 'ngx-material-extra';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgxMaterialExtraComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'demo-app';
